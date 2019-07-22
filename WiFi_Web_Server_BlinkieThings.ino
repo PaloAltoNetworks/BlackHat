d5392a46cd56d11dc35e7f5537566803d7b69b7b
malwaremama <34683779+malwaremama@users.noreply.github.com>
2019-07-22 15:40:01 -0400

    Update WiFi_Web_Server_BlinkieThings.ino

--- WiFi_Web_Server_BlinkieThings.ino
+++ WiFi_Web_Server_BlinkieThings.ino
@@ -15,19 +15,36 @@
 
 #include <WiFi.h>
 
-const char* ssid     = "<Enter SSID here>";
-const char* password = "<Enter WPA here>";
-int ext_led = 5;
+const char* ssid     = "<SSID>"; // Enter in the SSID you want to connect to
+const char* password = "<Password>"; // Enter the password for the SSID
+
+//const char* www_username = "admin"; // ***This is a default value change this!***
+//const char* www_password = "password"; // ***This is a default value change this!***
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+//Variable to store the HTTP request
+String header;
+
+// Auxiliar variables to store the current output state
+String LEDstate = "Off";
+
+// Assign variables
+const int ext_led = 5;
 int timer = 500;
 int i;
-
-WiFiServer server(80);
 
 void setup()
 {
     Serial.begin(115200);
-    pinMode(5, OUTPUT);
-    pinMode(BUILTIN_LED, OUTPUT); //set the internal LED pin mode
+    // Initialize the output variables as outputs
+    pinMode(ext_led, OUTPUT);
+    pinMode(BUILTIN_LED, OUTPUT);
+
+    // Set outputs to LOW
+    digitalWrite(ext_led, LOW);
+    digitalWrite(BUILTIN_LED, LOW);
                  
     delay(10);
 
@@ -49,9 +66,9 @@ void setup()
     Serial.println("WiFi connected.");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
+    Serial.print("/ in your browser to see it working");
     
-    server.begin();          
-  
+    server.begin();
 }
 
 int value = 0;
@@ -66,6 +83,7 @@ void loop(){
       if (client.available()) {             // if there's bytes to read from the client,
         char c = client.read();             // read a byte, then
         Serial.write(c);                    // print it out the serial monitor
+        header += c;
         if (c == '\n') {                    // if the byte is a newline character
 
           // if the current line is blank, you got two newline characters in a row.
@@ -77,8 +95,41 @@ void loop(){
             client.println("Content-type:text/html");
             client.println();
 
-            // the content of the HTTP response follows the header:
-            client.print("Click <a href=\"/alert\">here</a> to test LEDs<br>");
+            // turns the LEDs on and off
+            if (header.indexOf("GET /LED/on") >= 0) {
+              Serial.println("LEDs on");
+              LEDstate = "On";
+              digitalWrite(ext_led, HIGH);
+              digitalWrite(BUILTIN_LED, HIGH);
+            } else if (header.indexOf("GET /LED/off") >= 0) {
+              Serial.println("LEDs off");
+              LEDstate = "Off";
+              digitalWrite(ext_led, LOW);
+              digitalWrite(BUILTIN_LED, LOW);
+            } 
+              
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #555555;}</style></head>");
+            // Web Page Heading
+            client.println("<body><h1>DuckDuckSOC Web Server</h1>");
+            // Display current state, and ON/OFF buttons for ext_led  
+            client.println("<p>LED State: " + LEDstate + "</p>");
+            // If the LEDstate is off, it displays the ON button       
+            if (LEDstate=="off") {
+              client.println("<p><a href=\"/LED/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/LED/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+            client.println("</body></html>");
+
             // The HTTP response ends with another blank line:
             client.println();
             // break out of the while loop:
@@ -90,7 +141,7 @@ void loop(){
           currentLine += c;      // add it to the end of the currentLine
         }
 
-        // Check to see if the client request was "GET /alert":
+        // Check to see if the client request was "GET /alert" for NGFW HTTP POST:
         for (i = 0; i<=5; i++) {
           if (currentLine.endsWith("GET /alert")) 
           {
@@ -110,7 +161,9 @@ void loop(){
       }
     }
     // close the connection:
+    header = "";
     client.stop();
     Serial.println("Client Disconnected.");
+    Serial.println("");
   }
 }
